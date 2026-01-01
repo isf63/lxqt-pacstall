29a45bc0049066106c338db75d10189386739849
Ian Farrell <ianfarrell47@gmail.com>
2026-01-01 08:06:53 -0500

    Update

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-
-[[ "$PWD" = */lxqt-pacstall ]] || exit 1
-
-# for pkg in ./pkgbuild/*; do
-	# find "$pkg" -maxdepth 1 ! -wholename "$pkg" ! -name "PKGBUILD" -exec sudo rm -rf {} +
-	# sed -i '/^pkgver=/ s/\.r.*$//' "$pkg"/PKGBUILD
-# done
